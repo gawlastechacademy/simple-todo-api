af2f801c86337fb5e3b487dced9ff28ffd8c434c
Aga Siwek <176235224+aga-siwek@users.noreply.github.com>
2024-12-10 11:55:42 +0100

    STA-9: implement routes by flask blueprints

--- src/app.py
+++ src/app.py
@@ -1,101 +1,19 @@
-from flask import Flask, request
-import src.services.todo_service as todo_service
-import src.services.user_service as user_service
+from flask import Flask
 from src.database import db
-from flask_jwt_extended import JWTManager, jwt_required
+from flask_jwt_extended import JWTManager
+from src.routes.todo_route import todo_app
+from src.routes.user_route import user_app
 
 app = Flask(__name__)
+app.register_blueprint(todo_app)
+app.register_blueprint(user_app)
+
 app.config.from_object("config.Config")
 db.init_app(app)
 jwt = JWTManager(app)
 
 with app.app_context():
     db.create_all()
-
-
-@app.route("/register/", methods=["POST"])
-def register():
-    post_data = request.json
-    user_name = post_data["user"]
-    user_password = post_data["password"]
-    return user_service.create_user(user_name, user_password)
-
-
-@app.route("/login/", methods=["POST"])
-def login():
-    post_data = request.json
-    user_name = post_data["user"]
-    user_password = post_data["password"]
-    return user_service.login(user_name, user_password)
-
-
-@app.route("/todos/", methods=["GET"])
-@jwt_required()
-def list_todos():
-    return todo_service.get_all_todo()
-
-
-@app.route("/todos/", methods=["POST"])
-@jwt_required()
-def add_todo():
-    post_data = request.json
-    title = post_data["title"]
-    description = post_data["description"]
-    status = post_data["status"]
-    do_time = post_data["do_time"]
-    return todo_service.add_single_todo(title, description, status, do_time)
-
-
-@app.route("/todos/<task_id>", methods=["GET"])
-@jwt_required()
-def get_single_task(task_id):
-    return todo_service.get_single_todo(task_id)
-
-
-@app.route("/todos/<task_id>", methods=["DELETE"])
-@jwt_required()
-def delete_single_task(task_id):
-    return todo_service.delete_single_todo(task_id)
-
-
-@app.route("/todos/<task_id>", methods=["PUT"])
-@jwt_required()
-def update_single_task(task_id):
-    post_request = request.json
-    return todo_service.update_single_todo(post_request, task_id)
-
-
-# return all tasks from one user
-@app.route("/users/<user_id>/todos/", methods=["GET"])
-@jwt_required()
-def todos_user(user_id):
-    return todo_service.get_user_todos(user_id)
-
-
-@app.route("/users/", methods=["GET"])
-@jwt_required()
-def all_users():
-    return user_service.get_all_users()
-
-
-@app.route("/users/<user_id>", methods=["GET"])
-@jwt_required()
-def single_user(user_id):
-    return user_service.get_single_user(user_id)
-
-
-@app.route("/users/<user_id>", methods=["PUT"])
-@jwt_required()
-def change_user(user_id):
-    post_request = request.json
-    return user_service.change_single_user(post_request, user_id)
-
-
-@app.route("/users/<user_id>", methods=["DELETE"])
-@jwt_required()
-def delete_user(user_id):
-    return user_service.delete_single_user(user_id)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
